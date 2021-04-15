ea55c7f70d6da69dcbd7bc8228abc8df47cae0f3
Anderson TOPPER Dapper <topper.mitz@gmail.com>
2021-04-15 14:12:07 -0300

    Ajustes no layout

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Footer({dateString}){
   return (<footer className={styles.footer}>
             <div><h6>Última atualização {dateString}</h6></div>
-            <div><a
+            {/*<div><a
                   href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -12,7 +12,7 @@ export default function Footer({dateString}){
                   Powered by{' '}
                   <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
                 </a>
-            </div>
+            </div> */}
           </footer>
   )}
   
