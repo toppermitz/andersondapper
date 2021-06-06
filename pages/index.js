545c332ca5de53c7cfc04a87ede06c2c90bdea8a
Anderson Dapper <topper.mitz@gmail.com>
2021-06-06 16:47:28 -0300

    Alterando

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home({lastUpdate}) {
   return (
     <Layout dataString={lastUpdate}>
       <div className={styles.card}>
-        <p>Apaixonado por tecnologia, games e carros. Desde a adolescência brinquei com programação. Meu primeiro contato com esse mundo foi com Clipper.</p>
+        <p>Apaixonado por tecnologia, games e carros. Desde a adolescência brinquei com programação.Meu primeiro contato com esse mundo foi com Clipper.</p>
         <p>Em 2001 comecei a trabalhar com um tal Delphi, que rapidamente se tornou a minha principal ferramenta de trabalho.</p>
         <p>Atualmente sou desenvolvedor sênior na <a href="https://serverinfo.com.br/">Server Softwares para Varejo</a>.</p>
         <br />
