88c53e43d630f9f136511bc8be8602086ca1cf23
Anderson Dapper Rocha <topper.mitz@gmail.com>
2023-03-01 05:53:35 -0300

    chore: CI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,14 +24,15 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <script src="https://bshopatualizacao.s3.us-west-2.amazonaws.com/elastic-apm-rum.umd.min.js" crossorigin></script>
-    <script>
-      elasticApm.init({
-        serviceName: 'anderson-dapper',
-        serverUrl: 'https://acbb1e3ff16b40268668037b68ecf072.apm.us-west-2.aws.cloud.es.io:443',
-        environment: 'production',
-      })
-    </script>
+          <script async src="https://bshopatualizacao.s3.us-west-2.amazonaws.com/elastic-apm-rum.umd.min.js" crossOrigin='anonymous'/>
+          <script dangerouslySetInnerHTML={{
+              __html:`
+                  elasticApm.init({
+                    serviceName: 'anderson-dapper',
+                    serverUrl: 'https://acbb1e3ff16b40268668037b68ecf072.apm.us-west-2.aws.cloud.es.io:443',
+                    environment: 'production',
+                  })`}}/>
+          
           <Main />
           <NextScript />
         </body>
