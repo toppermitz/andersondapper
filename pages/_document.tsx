dff69acbd5f828d05a531393a004768a994e5efb
Anderson Dapper Rocha <topper.mitz@gmail.com>
2023-03-01 05:46:01 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <script src="https://bshopatualizacao.s3.us-west-2.amazonaws.com/elastic-apm-rum.umd.min.js" crossorigin></script>
+    <script>
+      elasticApm.init({
+        serviceName: 'anderson-dapper',
+        serverUrl: 'https://acbb1e3ff16b40268668037b68ecf072.apm.us-west-2.aws.cloud.es.io:443',
+        environment: 'production',
+      })
+    </script>
           <Main />
           <NextScript />
         </body>
